1e58319e4a33338ab3ba058743a830b17f877f0b
Emzy-Jayyy <emediongb23@gmail.com>
2025-11-03 23:40:54 +0100

    Update index.ts

--- companion-agent/src/mastra/index.ts
+++ companion-agent/src/mastra/index.ts
@@ -1,24 +1,24 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
-// import { weatherWorkflow } from "./workflows/weather-workflow";
-// import { weatherAgent } from "./agents/weather-agent";
+import { weatherWorkflow } from "./workflows/weather-workflow";
+import { weatherAgent } from "./agents/weather-agent";
 import { companionAgent } from "./agents/companion-agent";
-// import {
-//   toolCallAppropriatenessScorer,
-//   completenessScorer,
-//   translationScorer,
-// } from "./scorers/weather-scorer";
+import {
+  toolCallAppropriatenessScorer,
+  completenessScorer,
+  translationScorer,
+} from "./scorers/weather-scorer";
 import { a2aAgentRoute } from "./routes/a2a-agent-route";
 
 export const mastra = new Mastra({
-  // workflows: { weatherWorkflow },
-  agents: { companionAgent, CompanionAgent: companionAgent },
-  // scorers: {
-  //   toolCallAppropriatenessScorer,
-  //   completenessScorer,
-  //   translationScorer,
-  // },
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent, companionAgent, CompanionAgent: companionAgent },
+  scorers: {
+    toolCallAppropriatenessScorer,
+    completenessScorer,
+    translationScorer,
+  },
   storage: new LibSQLStore({
     // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
